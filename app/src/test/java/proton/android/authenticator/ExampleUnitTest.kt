d7dfeb314eebe392d6948b59fcec217cf5c9bd78
Victor Hidalgo <victor.hidalgo@proton.ch>
2025-02-10 08:49:24 +0100

    ci: add detekt to ci

--- app/src/test/java/proton/android/authenticator/ExampleUnitTest.kt
+++ app/src/test/java/proton/android/authenticator/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package proton.android.authenticator
 
+import junit.framework.TestCase.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
