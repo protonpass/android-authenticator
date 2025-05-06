29b4fb41d16f050e4a0b143c9695c4565b77b1c9
Alexander Talledo <alexander.talledo@proton.ch>
2025-05-06 11:45:14 +0200

    style: fix top bar action text button state colors

--- shared/ui/src/main/kotlin/proton/android/authenticator/shared/ui/domain/components/bars/SmallTopBar.kt
+++ shared/ui/src/main/kotlin/proton/android/authenticator/shared/ui/domain/components/bars/SmallTopBar.kt
@@ -22,12 +22,12 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import proton.android.authenticator.shared.ui.domain.components.buttons.TopBarActionTextButton
 import proton.android.authenticator.shared.ui.domain.models.UiIcon
 import proton.android.authenticator.shared.ui.domain.models.UiText
 import proton.android.authenticator.shared.ui.domain.theme.Theme
@@ -64,16 +64,11 @@ fun SmallTopBar(
         },
         actions = {
             action?.let { text ->
-                TextButton(
-                    enabled = isActionEnabled,
+                TopBarActionTextButton(
+                    text = text,
+                    isEnabled = isActionEnabled,
                     onClick = onActionClick
-                ) {
-                    Text(
-                        text = action.asString(),
-                        color = Theme.colorScheme.accent,
-                        style = Theme.typography.body1Medium
-                    )
-                }
+                )
             }
         },
         colors = TopAppBarDefaults.topAppBarColors(
--- shared/ui/src/main/kotlin/proton/android/authenticator/shared/ui/domain/components/textfields/FormTextField.kt
+++ shared/ui/src/main/kotlin/proton/android/authenticator/shared/ui/domain/components/textfields/FormTextField.kt
@@ -27,7 +27,7 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -47,7 +47,7 @@ fun FormTextField(
     isRequired: Boolean = false,
     isSingleLine: Boolean = true
 ) {
-    var value by remember(key1 = initialValue) {
+    var value by rememberSaveable {
         mutableStateOf(initialValue)
     }
 
