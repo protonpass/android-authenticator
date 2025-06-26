62b086a0651a995f388d3d85b1db0b9599395b29
Victor Hidalgo <victor.hidalgo@proton.ch>
2025-06-26 08:11:06 +0200

    bump: version 0.0.0 → 0.0.1

--- platform/build-logic/src/main/kotlin/proton/android/authenticator/platform/buildlogic/domain/platform/configuration/PlatformAndroidConfig.kt
+++ platform/build-logic/src/main/kotlin/proton/android/authenticator/platform/buildlogic/domain/platform/configuration/PlatformAndroidConfig.kt
@@ -25,7 +25,7 @@ internal object PlatformAndroidConfig {
 
     internal const val USES_COMPOSE: Boolean = true
 
-    internal const val VERSION_NAME: String = "0.1.0"
+    internal const val VERSION_NAME: String = "0.0.1"
 
     internal val VERSION_CODE: Int = VERSION_NAME.split('.')
         .map(String::toInt)
